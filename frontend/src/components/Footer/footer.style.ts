64c31764cb8194f0764ff53a2993d4d3e195b771
Lara Hassanieh <lara.kah@icloud.com>
2025-04-06 23:55:19 +0200

    Justerer litt på layout

--- frontend/src/components/Footer/footer.style.ts
+++ frontend/src/components/Footer/footer.style.ts
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
   width: 100%;
+    height:25vh;
   background-color: #003049;
   padding: 24px 40px;
+    margin: 0;
   display: flex;
   align-items: flex-start;     
   justify-content: flex-start; 
@@ -14,21 +16,23 @@ export const FooterContent = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-  margin-top: -60px; 
+  margin-top: -100px;
+   
 `;
 
 
 export const FooterLogo = styled.img`
   height: 200px;
   object-fit: contain;
-  margin-bottom: -65px;
-    transform: translateX(-100px);
+    margin-top:30px;
+    transform: translateX(-108px);
+    margin-bottom: -75px;
 
 `;
 
 export const FooterText = styled.p`
-  font-size: 1rem;
-  font-weight: 400;
+  font-size: 17px;
+  font-weight: 530;
   color: white;
   margin: 0;
   padding-top: 0; 
