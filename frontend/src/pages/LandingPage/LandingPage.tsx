93dde51cb6dbc1c40b62fb38460deb9b185fabcf
Sigurd Omnes <sigurd.omnes@nhn.no>
2025-05-22 10:05:16 +0200

    Fikser NAV -> Nav

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
     return (
         <section className={classes.section}>
             <h2 className={classes.welcomeText}>Har du mottatt en lenke fra oss?</h2>
-            <p className={classes.infoText}><span className={classes.logoText}>kort.nav.no</span> er en tjeneste levert av NAV. <br/> Har du mottatt en lenke fra oss og vil dobbelsjekke denne? <br/> Lim den inn i søkefeltet under!</p>
+            <p className={classes.infoText}><span className={classes.logoText}>kort.nav.no</span> er en tjeneste levert av Nav. <br/> Har du mottatt en lenke fra oss og vil dobbelsjekke denne? <br/> Lim den inn i søkefeltet under!</p>
             <CheckShortUrl/>
         </section>
     )
